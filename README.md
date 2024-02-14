0c870fbdbdba09950810987377dea1b91e98a701
SachaBlue <sdoukhan94@gmail.com>
2024-02-14 10:59:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+url de mon site : https://etudiant.u-pem.fr/~doukhancourtal/portrait-chinois/
+
+lien du tableau opquast : 
+
+Pour XAMPP, j'ai installé Apache, puis navigué vers "explorer" --> "htdocs", où j'ai placé mon dossier "Portrait Chinois". Grâce à cela, j'ai maintenant accès à mon site web via le lien sur Internet : http://localhost/Portrait%20Chinois/
---
+++ data.js/data.js
@@ -0,0 +1,55 @@
+// let analogie = ["une couleur", "un pays", "un endroit", "un loisir" , "une série" , "un jeu video", "un acteur"]
+
+ let valeurAnalogies = ["le bleu", "le japon", "mon jardin", "la creation de contenu" , "Better Call Saul" , "The Legend of Zelda Breath of the wild", "Keanu Reeves"]
+
+let data = [
+  {
+    "id": "couleur",
+    "analogie" : "une couleur, ", 
+    "valeurAnalogie" : "le bleu",
+    "description" : "Symbole de calme et de paix interieur mais aussi esthétiquement / visuelement attrayant ",
+    "img" : "https://images.assetsdelivery.com/compings_v2/bravissimos/bravissimos2006/bravissimos200600127.jpg",
+  },
+  {
+    "id": "pays",
+    "analogie" : "un pays, ", 
+    "valeurAnalogie" : "le japon",
+    "description" : "Encourageant ma façon de faire et d'être avec les gens , ce peuple posède un sens du civisme et de respect motivant ",
+    "img" : "https://assets.puzzlefactory.com/puzzle/328/976/original.webp"
+  },
+  {
+    "id": "endroit",
+    "analogie" : "un endroit, ", 
+    "valeurAnalogie" : "mon jardin",
+    "description" : "Lieu ou je puise mon élan créateur a travers la nature verdoyante",
+    "img" : "https://static6.depositphotos.com/1078879/660/i/450/depositphotos_6603793-stock-photo-english-garden.jpg"
+  },
+  {
+    "id": "loisir",
+    "analogie" : "un loisir, ", 
+    "valeurAnalogie" : "la creation de contenu",
+    "description" : "Domaine qui permet de me libérer, partager différentes choses et m'aide a gagner en confiance",
+    "img" : "https://media.istockphoto.com/id/1213200051/fr/photo/jeune-homme-amical-vlogging.jpg?s=612x612&w=0&k=20&c=c2t6a8WbMKBgoFnHhVy7xnmRTVFrHnswy9rFVywFY9M="
+  },
+  {
+    "id": "série",
+    "analogie" : "une série, ", 
+    "valeurAnalogie" : "Better Call Saul",
+    "description" : "Inspirant de la société d'aujourd'hui pour réussir et se faire connaître , transmettant des valeurs fortes",
+    "img" : "https://larepublica.cronosmedia.glr.pe/original/2022/04/15/6259ecfc8acb803bc55cc41a.jpg"
+  },
+  {
+    "id": "jeuVidéo",
+    "analogie" : "un jeu vidéo, ", 
+    "valeurAnalogie" : "The Legend of Zelda Breath of the wild",
+    "description" : "Environnement vaste remplie d'action, de quêtes et reposant ",
+    "img" : "https://upload.wikimedia.org/wikipedia/fr/thumb/9/90/The_Legend_of_Zelda_Breath_of_the_Wild_Logo.png/640px-The_Legend_of_Zelda_Breath_of_the_Wild_Logo.png"
+  },
+  {
+    "id": "acteur",
+    "analogie" : "un acteur, ", 
+    "valeurAnalogie" : "Keanu Reeves",
+    "description" : "Pour sa gentillese et sa brillance dans ses rôles",
+    "img" : "https://m1.quebecormedia.com/emp/emp/shutterstock_1410988244e60670ac-b4c8-46bc-973c-ef6aa26211b3_ORIGINAL.jpg?impolicy=crop-resize&x=0&y=167&w=2000&h=1126&width=1200"
+  }
+]
